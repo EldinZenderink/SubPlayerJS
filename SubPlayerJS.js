bed019ae7f5b16f0b476793e419a536a40c5b06c
EldinZenderink <s1077709@student.windesheim.nl>
2016-05-01 11:22:40 +0200

    jQuery

--- SubPlayerJS.js
+++ SubPlayerJS.js
@@ -13,7 +13,6 @@ var subtitleArray = [];
 var isSeeking = false;
 var video;
 
-
 !window.jQuery && document.write(unescape('%3Cscript src="https://code.jquery.com/jquery-2.1.1.min.js%3E%3C%2Fscript%3E'))
 /*
 try{
